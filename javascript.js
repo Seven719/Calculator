cc361a2dbbedb9a326bc564da73209735b69a747
Seven719 <ilie.andronie@gmail.com>
2023-11-20 10:11:05 +0000

    Added functionality to the calculator
    Rounded the decimal numbers to 11 decimal points so it doesn't exit the calculator
    
    Added previous calculation on top

--- javascript.js
+++ javascript.js
@@ -7,7 +7,10 @@ function performCalculation(operator, firstNumber, secondNumber) {
 		case 'x':
 		    return firstNumber * secondNumber;
 		case '÷':
-		    return firstNumber / secondNumber;
+            if (secondNumber === 0) return null
+            else return firstNumber / secondNumber;
+        default:
+            return null
 	}
 }
 
@@ -38,20 +41,24 @@ buttonContainer.appendChild(buttonClear);
 
 
 let buttonList = ['+', '=', '0', '.', '-', '3', '2', '1', 'x', '6', '5', '4', '÷', '9', '8', '7'];
+let operatorList = ['+', '-', '÷', 'x'];
 
 
 for(let i = buttonList.length - 1; i >= 0; i--) {
     let button = document.createElement('button');
     button.classList.add('buttons');
     button.value = buttonList[i];
+    if(operatorList.includes(button.value)) {
+        button.classList.add('operators');
+    }
     button.textContent = buttonList[i];
     buttonContainer.appendChild(button);
 }
+const buttonEquals = document.querySelector('button[value="="]');
+buttonEquals.id = 'equals-button';
 
 let numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let firstNumberList = [];
-let secondNumberList = [];
-let operatorList = ['+', '-', '÷', 'x'];
+let firstNumberList = [], secondNumberList = [];
 let operator = null;
 
 [...document.querySelectorAll('.buttons')].forEach(function(theButton) {
@@ -62,23 +69,49 @@ let operator = null;
         }
         currentOperation.textContent += theButton.textContent;
         if(operatorList.includes(theButton.textContent)) {
-            operator = theButton.textContent;
+            calculate();
+            operator = theButton.textContent; 
         }
         else if(operator == null) {
             firstNumberList.push(theButton.textContent);
         }
-        else {
+        else if(theButton.textContent != '='){
             secondNumberList.push(theButton.textContent);
         }
         screenContainer.appendChild(currentOperation);
     });
 });
 
-let firstNumber = Number(firstNumberList.join(''));
-let secondNumber = Number(secondNumberList.join(''));
+var result;
+
+function calculate() {
+    if (operator && secondNumberList.length > 0) {
+        let firstNumber = Number(firstNumberList.join(''));
+        let secondNumber = Number(secondNumberList.join(''));
+        result = Math.round(performCalculation(operator, firstNumber, secondNumber) * 100000000000) / 100000000000;
+
+        firstNumberList = [result.toString()];
+        secondNumberList = [];
+    }
+}
+
+buttonEquals.addEventListener('click', function () {
+    calculate();
+
+    lastOperation.textContent = currentOperation.textContent;
+    currentOperation.textContent = result;
+    
+    screenContainer.appendChild(lastOperation);
+    screenContainer.appendChild(currentOperation);
+});
 
 buttonClear.addEventListener('click', function() {
     currentOperation.textContent = '0';
+    lastOperation.textContent = '';
+    firstNumberList = [];
+    secondNumberList = [];
+    operator = null;
+    screenContainer.appendChild(lastOperation);
     screenContainer.appendChild(currentOperation);
 })
 
